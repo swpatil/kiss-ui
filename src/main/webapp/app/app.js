b9a5921ce3cd4ea4d6f1a0aa33cdc9b78c22f97c
subal <subal.chaudhari@capgemini.com>
2015-04-01 12:13:35 +0530

    Example

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -1,1 +1,10 @@
-var app = angular.module('app', ['ngRoute']);
+(function(){
+	var app = angular.module('kiss', []);
+	app.controller('StoreController',function(){
+		this.product = gem;
+	});
+	
+	var gem ={
+			name:'Test',price: 2.95,description:'Test description',canPurchase:false
+	};
+})();
