708300f26b48f4a5efb799efa27a2a278494cf06
Avinash-Solanki <avinash.solanki95@gmail.com>
2015-04-06 15:09:39 +0200

    tree.js has been modified to get the real data
    tree.js has been modified to get the real data

--- src/main/webapp/lib/angular/tree.js
+++ src/main/webapp/lib/angular/tree.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('treeApp', ['ui.tree'])
-  .controller('treeCtrl', function($scope) {
+  .controller('treeCtrl', function($scope,$http) {
 
     $scope.remove = function(scope) {
       scope.remove();
@@ -40,63 +40,14 @@
       scope.expandAll();
     };
 
-    $scope.data = [{
-      "id": 1,
-      "title": "node1",
-      "nodes": [
-        {
-          "id": 11,
-          "title": "node1.1",
-          "nodes": [
-            {
-              "id": 111,
-              "title": "node1.1.1",
-              "nodes": []
-            }
-          ]
-        },
-        {
-          "id": 12,
-          "title": "node1.2",
-          "nodes": []
-        }
-      ],
-    }, {
-      "id": 2,
-      "title": "node2",
-      "nodes": [
-        {
-          "id": 21,
-          "title": "node2.1",
-          "nodes": []
-        },
-        {
-          "id": 22,
-          "title": "node2.2",
-          "nodes": []
-        }
-      ],
-    }, {
-      "id": 3,
-      "title": "node3",
-      "nodes": [
-        {
-          "id": 31,
-          "title": "node3.1",
-          "nodes": []
-        }
-      ],
-    }, {
-      "id": 4,
-      "title": "node4",
-      "nodes": [
-        {
-          "id": 41,
-          "title": "node4.1",
-          "nodes": []
-        }
-      ],
-    }];
+	$http.get('http://localhost:8080/rest/cableunit/cu/0006018380').
+	  success(function(data, status, headers, config) {
+	 $scope.data = [data];
+	  }).
+	  error(function(data, status, headers, config) {
+	    // called asynchronously if an error occurs
+	    // or server returns response with an error status.
+	  });
   });
 
 })();
